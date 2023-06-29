2d200da2bfd9bdfabb81d175dce98d7f26187d5a
toto <riedotoine@gmail.com>
2023-06-29 21:13:38 +0200

    formatting blog post

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
-import { getStoredPostsData } from "../lib/posts";
+import { getSortedPostsData } from "../lib/posts";
 
 function myAge() {
   const BORN = new Date("2000-07-31");
@@ -24,15 +24,6 @@ function myAge() {
   return diffInYears;
 }
 
-export async function getStaticProps() {
-  const allPostsData = getStoredPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
-
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
@@ -40,20 +31,22 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>
-          Hello, I am Toine (but you can call me <b>toto</b>) I am {myAge()}yo
-          and I am a devops engineer. But I love learning new stuff and
-          particulary programmation. Web Development is something I love and why
-          not get a position some day as a developer :)
-        </p>
-        <p>
-          You can contact me on{" "}
-          <a href="https://twitter.com/Toine0805" target="_blank">
-            twitter
-          </a>{" "}
-          if you want.
+        <div>
+          <p>
+            Hello, I am Toine (but you can call me <b>toto</b>) I am {myAge()}yo
+            and I am a devops engineer. But I love learning new stuff and
+            particulary programmation. Web Development is something I love and
+            why not get a position some day as a developer.
+          </p>
+          <p>
+            You can contact me on{" "}
+            <a href="https://twitter.com/Toine0805" target="_blank">
+              twitter
+            </a>{" "}
+            if you want.
+          </p>
           <hr />
-        </p>
+        </div>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog posts</h2>
@@ -71,4 +64,13 @@ export default function Home({ allPostsData }) {
       </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
